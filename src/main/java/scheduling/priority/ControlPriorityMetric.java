784d876c3b03892b3e271c23249eedd7f8cee65b
Dimitris Palyvos Giannas <palyvos@chalmers.se>
2018-03-29 11:02:45 +0200

    Make the "control" metric do the work of calculating the actual priorities before replacing them with random ones.

--- src/main/java/scheduling/priority/ControlPriorityMetric.java
+++ src/main/java/scheduling/priority/ControlPriorityMetric.java
@@ -4,36 +4,25 @@ import java.util.List;
 import java.util.Random;
 
 import common.ActiveRunnable;
-import common.tuple.Tuple;
-import stream.Stream;
 
-public class ControlPriorityMetric extends PriorityMetric {
+public class ControlPriorityMetric extends QueueSizeMetric {
 
 	protected ControlPriorityMetric(List<ActiveRunnable> tasks, List<ActiveRunnable> passiveTasks) {
 		super(tasks, passiveTasks);
 	}
 
 	@Override
-	public <IN extends Tuple> void recordTupleRead(IN tuple, Stream<IN> input) {
-	}
-
-	@Override
-	public <OUT extends Tuple> void recordTupleWrite(OUT tuple, Stream<OUT> output) {
-	}
-
-	@Override
 	public List<Double> getPriorities(int scaleFactor) {
-		Random rand = new Random();
-		long[] priorities = new long[tasks.size()];
-		for (int i = 0; i < tasks.size(); i++) {
-			if (isIgnored(tasks.get(i))) {
-				priorities[i] = 0;
-				continue;
-			}
-			priorities[i] = rand.nextInt(100);
-
+		List<Double> priorities = super.getPriorities(scaleFactor);
+		Random r = new Random();
+		double sum = 0;
+		for (int i = 0; i < priorities.size(); i++) {
+			double p = isIgnored(tasks.get(i)) ? 0 : r.nextDouble();
+			priorities.set(i, p);
+			sum += p;
 		}
-		return scale(priorities, 1);
+		normalize(priorities, sum);
+		return priorities;
 	}
 
 }
