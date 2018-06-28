a1224fea8733afb98c4928cc576c2bde1551ff8b
Dimitris Palyvos Giannas <palyvos@chalmers.se>
2018-06-28 18:01:51 +0200

    Draft of the idea for very basic scheduler with given percentages of runtime for every task.

---
+++ src/main/java/scheduling/thread/PrescheduledThread.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017-2018
+ *   Vincenzo Gulisano
+ *   Dimitris Palyvos-Giannas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ *   Vincenzo Gulisano info@vincenzogulisano.com
+ *   Dimitris Palyvos-Giannas palyvos@chalmers.se
+ */
+
+package scheduling.thread;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.concurrent.TimeUnit;
+
+public class PrescheduledThread extends LiebreThread {
+
+  private final Deque<ExecutionEntry> schedule;
+  private final long quantum;
+  private final TimeUnit unit;
+
+  public PrescheduledThread(Collection<ExecutionEntry> schedule, long quantum,
+      TimeUnit unit) {
+    this.schedule = new ArrayDeque(schedule);
+    this.quantum = quantum;
+    this.unit = unit;
+  }
+
+  @Override
+  protected void doRun() {
+    ExecutionEntry task = schedule.removeFirst();
+    task.run(quantum, unit);
+    schedule.addLast(task);
+  }
+}
